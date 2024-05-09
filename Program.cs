26672c52e58a877bd765dd68c0e018cc48f85911
bensoil <157688179+bensoil@users.noreply.github.com>
2024-05-09 22:22:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿var numberInt = 1;
+
+var numberLong = 9223372036854775807;
+
+var numberDouble = 1.3;
+
+var numberFloat = 1.3F;
+
+var numberDeci = 1.3M;
+
+var stringendo = "This is reality, Matt";
+
+var charVar = 'A';
+
+var boolAttrib = true;
